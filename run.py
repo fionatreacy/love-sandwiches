3a710a3d17181a64973d9057ed4529bc19626dd0
fionatreacy <fionatreacy00@gmail.com>
2022-06-18 15:10:30 +0000

    collect sales data from last 5 markets

--- run.py
+++ run.py
@@ -53,23 +53,26 @@ def validate_data(values):
 Redundant code, refactored into updaet_worksheet
 
 def update_sales_worksheet(data):
-    """
+    
     update sales worksheet wiht new data on new row
-    """
+    
     print("updating sales worksheet...\n")
     sales_worksheet = SHEET.worksheet("sales")
     sales_worksheet.append_row(data)
     print("sales workseet updated successfully\n")
 
 def update_surplus_worksheet(data):
-    """
+   
     update surplus worksheet wiht new data on new row
-    """
+    
     print("updating surplus worksheet...\n")
     surplus_worksheet = SHEET.worksheet("surplus")
     surplus_worksheet.append_row(data)
     print("surplus workseet updated successfully\n")
-    """
+    
+
+"""
+
 
 def update_worksheet(data, worksheet):
     """
@@ -93,9 +96,22 @@ def calculate_surplus_data(sales_row):
     return(surplus_data)
 
 
+def get_last_5_entries_sales():
+    """
+    collects data from worksheet, takes last 5 data entries for each sandwich type and works out the mean amount sold rounding to nearest integer
+    """
+    sales = SHEET.worksheet("sales")
+
+    columns = []
+    for ind in range(1, 7):
+        column = sales.col_values(ind)
+        columns.append(column[-5:])
+    return columns
+
+
 def main():
     """
-    call all functions
+    calls all functions
     """
     data = get_sales_data()
     sales_data = [int(num) for num in data]
@@ -104,4 +120,7 @@ def main():
     update_worksheet(new_surplus_data, 'surplus')
 
 print("wlecome to love sandwiches")
-main()
+
+""" main()"""
+
+sales_columns = get_last_5_entries_sales()
